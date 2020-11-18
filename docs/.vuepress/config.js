f05ef5528e3b5e828224344c4b1ec68188b8ca80
Yuming Ma <mayum>
2020-11-18 18:39:08 +0800

    'sidebar'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,20 +1,22 @@
 module.exports = {
-    title: 'Yx\'s blog',
-    description: '杨兄的个人网站',
+    title: '杨兄的博客',
+    description: '个人技术指北',
     head: [ // 注入到当前页面的 HTML <head> 中的标签
       ['link', { rel: 'icon', href: '/logo.png' }], // 增加一个自定义的 favicon(网页标签的图标)
     ],
     base: '/', // 这是部署到github相关的配置
     markdown: {
-      lineNumbers: false // 代码块显示行号
+      lineNumbers: true // 代码块显示行号
     },
     themeConfig: {
       nav:[ // 导航栏配置
         {text: '前端', link: '/accumulate/' },
-        {text: '算法', link: '/algorithm/'},
-        {text: '微博', link: 'https://baidu.com'}      
+        {text: '算法', link: '/algorithm/'},    
       ],
-      sidebar: 'auto', // 侧边栏配置
+      sidebar: [
+        '/',
+        '/guide/'
+    ],
       sidebarDepth: 2, // 侧边栏显示2级
     }
   };
